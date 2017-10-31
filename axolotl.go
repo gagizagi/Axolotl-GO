cd3fe5f6e94a497ebf997506b07839a7d4feb63c
GazZy <gagizagi@gmail.com>
2017-10-31 20:01:31 +0100

    Defer HTTP server and move to .env

--- axolotl.go
+++ axolotl.go
@@ -3,29 +3,30 @@ package main
 import (
 	"os"
 	"time"
+
+	_ "github.com/joho/godotenv/autoload"
 )
 
 var ircClient = ircConfig{
 	Server:   "irc.rizon.net:6667",
 	Channels: []string{"#422", "#HORRIBLESUBS"},
-	Nickname: "Axolotl-moe",
+	Nickname: os.Getenv("AX_IRC_NICKNAME"),
 	Verbose:  false,
 	Debug:    false,
 }
 
 var discordClient = discordConfig{
-	Boss:         os.Getenv("DISCORD_BOSS"),
-	Token:        os.Getenv("DISCORD_TOKEN"),
+	Boss:         os.Getenv("AX_DISCORD_BOSS"),
+	Token:        os.Getenv("AX_DISCORD_TOKEN"),
 	AnimeChannel: "anime",
 	Debug:        false,
 }
 
 func main() {
+	defer webServer()
 	dbConn()
 
 	go ircConnStart(&ircClient)
 	go discordStart(&discordClient)
 	go maintainAnimeListProcess(10 * time.Hour)
-
-	webServer() //Last
 }
