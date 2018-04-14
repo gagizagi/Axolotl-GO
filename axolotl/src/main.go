fecb56908bafc317f42fb2e57d952b7f3d61ce3a
GazZy <gagizagi@gmail.com>
2018-04-14 14:56:47 +0200

    avatar upload

--- axolotl/src/main.go
+++ axolotl/src/main.go
@@ -8,7 +8,7 @@ import (
 var discordClient = discordConfig{
 	Boss:         os.Getenv("DISCORD_BOSS"),
 	Token:        os.Getenv("DISCORD_TOKEN"),
-	AvatarURL:    "https://camo.githubusercontent.com/c40a9a73cc03b760a567df127d7fcebb59724580/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f617661746172732f3138353137373835313739393031313332392f37306336653365396135373633626564396664663336353130653831323733612e6a7067",
+	AvatarURL:    "https://axolotl.gazzy.online/static/axolotl.jpg",
 	AnimeChannel: "anime",
 	Debug:        false,
 }
